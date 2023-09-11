3fc2de06814528524edf7b5a024b458089b897b6
emmamkb <emmanuelmakabu7@gmail.com>
2023-09-12 01:06:09 +0200

    ✨ Add controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class HomeController extends Controller
+{
+    //
+    function subscribe_to_newsletter() : View {
+        return view('home')->with('message', 'Souscription reussie');
+    }
+}
